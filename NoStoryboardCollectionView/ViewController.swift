f61ad822ffc09d10c8f1fe1d9361120d7bc905a3
issei.ueda <ron.ron633662@gmail.com>
2024-05-12 08:13:53 +0900

    first commit

---
+++ NoStoryboardCollectionView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NoStoryboardCollectionView
+//
+//  Created by Issei Ueda on 2024/05/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .blue
+    }
+
+
+}
+
